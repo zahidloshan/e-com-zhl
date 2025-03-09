41a3df23b8ca5ebc683421ae0c02d3b8e3e70ddd
Zahid Hasan Loshan <75216112+zahidloshan@users.noreply.github.com>
2025-03-10 01:05:18 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import CardSlider from "@/components/CardSlider";
 import Products from "@/components/Products";
 import Footer from "@/components/Footer";
 
@@ -9,7 +8,7 @@ export default function Home() {
         <div className=" px-6 py-2 bg-gray-100">
             <Navbar />
             <Hero />
-            {/* <CardSlider /> */}
+
             <Products />
             <Footer />
         </div>
